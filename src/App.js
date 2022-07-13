a75637a94715e3ae78c93ae5d6cd19a55e3595a7
Robert J <74118301+KermitTheSapo@users.noreply.github.com>
2022-07-13 08:39:58 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React, { Component } from "react";
+
+export default class nome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Prática React 02 - Component de Class</h1>
+        <p>Robert José</p>
+      </div>
+    );
+  }
+}
